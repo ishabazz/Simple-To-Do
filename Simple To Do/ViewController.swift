935b82dc580eda0d91af7433604d949710cc04d9
Ish <Ish@illuminatedbits.com>
2016-07-16 09:41:55 -0700

    Initial Commit

---
+++ Simple To Do/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple To Do
+//
+//  Created by Demo on 7/16/16.
+//  Copyright © 2016 Illuminated Bits LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
